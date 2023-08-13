aa645f30aeca46ecccd9fad3b96cfc14744353ba
Morteza <khodemousa@gmail.com>
2023-08-13 23:28:38 +0330

    Email configuration

--- app/Http/Controllers/Seller/SellerProductController.php
+++ app/Http/Controllers/Seller/SellerProductController.php
@@ -7,6 +7,7 @@ use App\Models\Product;
 use App\Models\Seller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class SellerProductController extends ApiController
@@ -33,7 +34,7 @@ class SellerProductController extends ApiController
         );
         $data = $request->all();
         $data['status'] = Product::UNAVAILABLE_PRODUCT;
-        $data['image'] = '1.jpg';
+        $data['image'] = $request->image->store('');
         $data['seller_id'] = $seller->id;
         $product = Product::create($data);
 
@@ -59,6 +60,10 @@ class SellerProductController extends ApiController
                 return $this->errorResponse('An active product must have at least one category', 409);
             }
         }
+        if ($request->hasFile('image')) {
+            Storage::delete($product->image);
+            $product->image = $request->image->store('');
+        }
 
         if (!$product->isDirty()) {
             return $this->errorResponse('you need to specify a different value to update', 422);
@@ -72,6 +77,7 @@ class SellerProductController extends ApiController
         $this->checkSeller($seller, $product);
 
         $product->delete();
+        Storage::delete($product->image);
         return $this->showOne($product);
     }
     protected function checkSeller(Seller $seller, Product $product)
--- config/filesystems.php
+++ config/filesystems.php
@@ -39,9 +39,14 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
+        ],
+        'images' => [
+            'driver' => 'local',
+            'root' => public_path('img'),
+            'visibility' => 'public',
         ],
 
         's3' => [
